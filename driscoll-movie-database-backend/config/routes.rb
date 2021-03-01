466ca22adc6a2c1212b656258360d3aef11ba69f
Jake <jrdriscoll17@gmail.com>
2021-03-01 16:21:54 -0600

    Changed routing to make it cleaner

--- driscoll-movie-database-backend/config/routes.rb
+++ driscoll-movie-database-backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope '/api' do
-    resources :users
-    resources :movies, except: %i[update, edit]
+    resources :users do
+      resources :movies, only: %i[index create new destroy]
+    end
   end
 end
