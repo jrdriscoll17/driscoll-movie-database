bf2e89d2b4fe6e0bbd3a8e75d4f6896e6ffe1eca
Jake <jrdriscoll17@gmail.com>
2021-03-01 16:57:43 -0600

    Basic page templates setup

--- driscoll-movie-database-frontend/src/App.js
+++ driscoll-movie-database-frontend/src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import './App.css';
 
-function App() {
-  return (
-    <>
-      <NavBar />
-    </>
-  );
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={home}>
+            Home
+          </Route>
+          <Route exact path='/login' component={login}>
+            Login
+          </Route>
+          <Route exact path='/signup' component={signup}>
+            Sign Up
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ driscoll-movie-database-frontend/src/pages/signup.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class signup extends Component {
+  render() {
+    return <h1>Signup</h1>;
+  }
+}
+
+export default signup;
