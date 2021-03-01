63fa9aae8ced5d6ae207417552280dc48c6db249
Jake <jrdriscoll17@gmail.com>
2021-03-01 06:54:27 -0600

    Added User resource

---
+++ driscoll-movie-database-backend/app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def index; end
+  def show; end
+  def new; end
+  def create; end
+  def update; end
+  def edit; end
+  def delete; end
+end
--- driscoll-movie-database-backend/config/routes.rb
+++ driscoll-movie-database-backend/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :users
   resources :movies, except: %i[update, edit]
 end
