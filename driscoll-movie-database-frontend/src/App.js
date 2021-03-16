e20590a89abfa2f86dbe2a9488b21ea40f3b5f59
Jake <jrdriscoll17@gmail.com>
2021-03-16 16:44:51 -0500

    Converted to parcel and began implementation of material ui

--- driscoll-movie-database-frontend/src/App.js
+++ driscoll-movie-database-frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
 import Home from './components/Home';
 import Movies from './components/movies/Movies';
 import NavBar from './components/nav/NavBar';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './util/theme';
+import store from './store';
 
-const App = () => {
+export default function App() {
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/movies' component={Movies} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/movies' component={Movies} />
+        </Switch>
+      </Provider>
+    </ThemeProvider>
   );
-};
-
-export default App;
+}
---
+++ driscoll-movie-database-frontend/src/util/theme.js
@@ -0,0 +1,7 @@
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+const themeObject = {};
+
+const theme = createMuiTheme(themeObject);
+
+export default theme;
