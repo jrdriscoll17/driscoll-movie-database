6f0a44ca71c1ff253c4e6be2fa3761175dd78b6c
Jake <jrdriscoll17@gmail.com>
2021-03-16 09:07:31 -0500

    Converted movies component into a functional component

--- driscoll-movie-database-frontend/src/components/movies/Movies.js
+++ driscoll-movie-database-frontend/src/components/movies/Movies.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import './Movie.css';
 import Movie from './Movie';
 
-class Movies extends Component {
-  render() {
-    return (
-      <div className='movie-list-container'>
-        <ul className='movie-list'>
-          {this.props.movies.map((movie, index) => (
-            <li key={index} className='movie-list-item'>
-              <Movie movie={movie} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const Movies = props => {
+  return (
+    <div className='movie-list-container'>
+      <ul className='movie-list'>
+        {props.movies.map((movie, index) => (
+          <li key={index} className='movie-list-item'>
+            <Movie movie={movie} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ movieReducer: { movies } }) => {
   return {
